9ded78be5767ba9ed4accb5ab802aa0d08f1912a
Ayush Gupta <ayushgupta3902@gmail.com>
2024-06-09 14:50:01 +0530

    feat: add community structure

---
+++ lib/models/community.model.ts
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const communitySchema = new mongoose.Schema({
+  id: { type: String, required: true },
+  username: { type: String, required: true, unique: true },
+  name: { type: String, required: true },
+  image: String,
+  bio: String,
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  posts: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Post",
+    },
+  ],
+  members: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+});
+
+export const Community =
+  mongoose.models.Community || mongoose.model("Community", communitySchema);
